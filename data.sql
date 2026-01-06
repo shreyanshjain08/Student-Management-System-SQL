c4b7378eacd27409139e6b9151e8262286896296
Shreyansh Jain <143748940+shreyanshjain08@users.noreply.github.com>
2026-01-06 14:41:24 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,12 @@
+INSERT INTO students (name, email, department) VALUES
+('Amit Sharma', 'amit@gmail.com', 'CSE'),
+('Neha Verma', 'neha@gmail.com', 'IT');
+
+INSERT INTO courses (course_name, credits) VALUES
+('Database Systems', 4),
+('Data Structures', 3);
+
+INSERT INTO enrollments (student_id, course_id, marks) VALUES
+(1, 1, 85),
+(1, 2, 78),
+(2, 1, 90);
